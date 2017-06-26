7617004c9c589513baf3d9e0242f0a94b338ff03
= <georgegu1997@gmail.com>
2017-06-26 17:19:39 +0800

    add text to the map

--- src/components/RouteDrawer.vue
+++ src/components/RouteDrawer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="root">
-    <svg weight="300" height="300">
+    <svg weight="500" height="500">
     </svg>
   </div>
 </template>
@@ -14,7 +14,8 @@ export default {
   data() {
     return {
       ROAD: [],
-      ROUTES_DISTANCE: {}
+      ROUTES_DISTANCE: {},
+      GATE: []
     }
   },
   mounted() {
@@ -30,24 +31,55 @@ export default {
       that.ROUTES_DISTANCE = d
       that.drawRoutePath("camping1", "general-gate5")
     })
+
+    d3.csv("static/data/sensor_position_1.csv", function(d) {
+      that.GATE = d
+      that.drawGate()
+    })
+
+    d3.json("static/data/routes.json", function(d) {
+      console.log("Load routes finished");
+      that.drawRoutes([d[0]])
+    })
   },
   methods: {
     initialize() {
       var root = d3.select(".root")
       this.svg = root.select("svg")
+        .style("width", 500)
+        .style("height", 500);
 
       var weight = this.svg.attr("weight"),
           height = this.svg.attr("height")
 
       this.x_scale = d3.scaleLinear()
-                      .domain([0, 200])
-                      .range([0, weight])
+        .domain([0, 200])
+        .range([0, weight])
       this.y_scale = d3.scaleLinear()
-                      .domain([0, 200])
-                      .range([height, 0])
+        .domain([0, 200])
+        .range([height, 0])
+
+      this.setupMarker()
     },
+
+    setupMarker() {
+      this.svg.append("svg:defs").append("svg:marker")
+        .attr("id", "triangle")
+        .attr("refX", 6)
+        .attr("refY", 6)
+        .attr("markerWidth", 50)
+        .attr("markerHeight", 50)
+        .attr("markerUnits","userSpaceOnUse")
+        .attr("orient", "auto")
+        .append("path")
+        .attr("d", "M 0 0 12 6 0 12 3 6")
+        .style("fill", "red");
+    },
+
     drawRoad() {
-      var road = this.svg.append("g")
+      var road = this.svg.selectAll(".road")
+        .data(["road"])
+        .enter().append("g")
         .attr("class", "road")
 
       road.selectAll("circle")
@@ -60,18 +92,108 @@ export default {
         .attr("cy", d => this.y_scale(d[1]));
     },
 
+    drawGate() {
+      var gate = this.svg.selectAll(".gate")
+        .data(["gate"])
+        .enter().append("g")
+        .attr("class", "gate")
+
+      var group = gate.selectAll(".gate-group")
+        .data(this.GATE)
+        .enter()
+        .append("g")
+        .attr("class", "gate-group")
+
+      group.append("circle")
+        .style("fill", "blue")
+        .attr("r", 3)
+        .attr("cx", d => this.x_scale(d.x))
+        .attr("cy", d => this.y_scale(d.y))
+
+      group.append("text")
+        .attr("x", d => this.x_scale(d.x))
+        .attr("y", d => this.y_scale(d.y))
+        .text(d => d["gate-name"])
+        .attr("text-anchor", "middle")
+    },
+
     drawRoutePath(from_position, to_position) {
       var data = this.ROUTES_DISTANCE[from_position][to_position].break_points
-      console.log(data);
+      var svg = this.svg
 
       var valueline = d3.line()
-          .x(d => this.x_scale(d[0]))
-          .y(d => this.y_scale(d[1]))
+        .x(d => this.x_scale(d[0]))
+        .y(d => this.y_scale(d[1]))
+/*
+      var path = this.svg.selectAll(".route-path")
+        .data(["route-path"])
+        .enter().append("g")
+        .attr("class", "route-path")
 
-      this.svg.append("path")
-          .data([data])
-          .attr("class", "line")
-          .attr("d", valueline)
+      // path
+      path.append("path")
+        .data([data])
+        .attr("class", "line")
+        .attr("d",valueline)
+        .attr("marker-end", "url(#triangle)")
+        .attr("stroke", "grey")
+        .attr("stroke-width", "1.5")
+        .attr("fill", "transparent")
+*/
+      var path = this.svg.selectAll(".route-path")
+        .data([data])
+        .enter().append("g")
+        .attr("class", "route-path")
+
+      // path
+      path.append("path")
+        .attr("class", "line")
+        .attr("d",valueline)
+        .attr("marker-end", "url(#triangle)")
+        .attr("stroke", "grey")
+        .attr("stroke-width", "1.5")
+        .attr("fill", "transparent")
+    },
+
+    //routes should be a list of routes
+    //route should be a list of gate-names that the route pass
+    drawRoutes(route_list) {
+
+      for (var i = 0; i < route_list.length; i ++) {
+        route_list[i].paths = []
+        for (var j = 0; j < route_list[i].records.length - 1; j ++) {
+          route_list[i].paths.push([route_list[i].records[j], route_list[i].records[j+1]])
+        }
+      }
+
+      var svg = this.svg
+
+      var valueline = d3.line()
+        .x(d => {console.log(d.revords);})
+        .y(d => this.y_scale(d[1]))
+
+      var routes = svg.selectAll(".routes")
+        .data([route_list])
+        .enter().append("g")
+        .attr("class", "routes")
+
+      var route_group = routes.selectAll(".route-group")
+        .data(route_list)
+        .enter().append("g")
+        .attr("class", "route-group")
+
+      route_group.append("g")
+        .attr("class", "paths")
+        .datum(d => d.paths)
+        .enter().append("path")
+        .attr("class", "line")
+        .attr("x", d => {console.log(d);})
+        .attr("d", valueline)
+        .attr("marker-end", "url(#triangle)")
+        .attr("stroke", "grey")
+        .attr("stroke-width", "1.5")
+        .attr("fill", "transparent")
+
     }
   }
 
@@ -83,5 +205,9 @@ export default {
   fill: none;
   stroke: red;
   stroke-width: 3px;
+}
+
+.gate-group text {
+  font-size: 7px;
 }
 </style>
